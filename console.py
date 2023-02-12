deb18190048ee90aca3d5b6fd1d6ca5fddb9a01b
seguser <gbodisegun@gmail.com>
2023-02-12 06:32:02 -0800

    fix typo error in error message

--- console.py
+++ console.py
@@ -126,7 +126,7 @@ class HBNBCommand(cmd.Cmd):
         key = f"{cls_name}.{id}"
         my_models = storage.all()
         if key not in my_models:
-            print("**no instance foune **")
+            print("**no instance found **")
             return
         if not attr_name:
             print("** attribute name missing **")
